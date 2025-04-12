5a957d859d5e75edc89fc43bff20297fddf0b213
BARAMEE RUJIPATANAPONG <baramee.rujipatanapong@gmail.com>
2025-04-12 16:12:31 +1000

    refactor: Remove unused grid section variable from crime report form

--- src/components/crimeReportForms.tsx
+++ src/components/crimeReportForms.tsx
@@ -45,8 +45,6 @@ export default function CrimeReportForm() {
   const [personType, setPersonType] = React.useState<string>("");
   const [crimeGender, setCrimeGender] = React.useState<string>("");
   const [crimeInitial, setCrimeInitial] = React.useState<string>("");
-  const gridSection =
-    "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4";
   return (
     <>
       <div className="max-w-6xl mx-auto p-4 bg-white shadow-md rounded-lg mt-10 mb-10">

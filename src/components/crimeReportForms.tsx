99a7d340a3113bf8372f94cd5615df710564b36d
BARAMEE RUJIPATANAPONG <baramee.rujipatanapong@gmail.com>
2025-04-12 16:12:14 +1000

    refactor: Remove unused imports from crime report form component

--- src/components/crimeReportForms.tsx
+++ src/components/crimeReportForms.tsx
@@ -4,7 +4,6 @@ import TextField from "@mui/material/TextField";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
-import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import dayjs from "dayjs";
 import "dayjs/locale/th"; // Import Thai locale for dayjs
 import { DatePicker } from "@mui/x-date-pickers/DatePicker/DatePicker";
